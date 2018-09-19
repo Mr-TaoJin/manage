b1ffae9454edba01eed9df001e5a2ad6db19742a
陶锦 <312090373@qq.com>
2018-09-19 14:34:28 +0800

    标准模板的引入

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# manage
-VUE后台管理
-＃测试本机同时使用Gitlab和Githu是否冲突
+# 前端环境搭建
+# manage VUE后台管理
+# 项目环境搭建说明(先安装淘宝对象)
+# 安装node.js
+# npm install -g cnpm --registry=https://registry.npm.taobao.org (npm淘宝对象)
+# cnpm install --global vue-cli
+# vue init webpack <project-name>
+# cnpm install
+# cnpm install --save vue element-ui
+# cnpm install --save vue-router
+# cnpm install vuex --save
+# cnpm install --save axios
+# cnpm install sass-loader -D
+# cnpm install eslint --save-dev  //检测代码是否规范
---
+++ web/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+
+import 'normalize.css/normalize.css' // css重置
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n 中英文切换
+
+import '@/styles/index.scss' // 全局css样式
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import '@/icons' // 图标
+import '@/permission' // 权限管理
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
